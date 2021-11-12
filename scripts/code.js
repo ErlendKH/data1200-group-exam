d39fb683d3812083ca148700a2fb51d19cfb0a71
marcioziomek <s366436@oslomet.no>
2021-11-12 03:03:15 +0100

    .

--- scripts/code.js
+++ scripts/code.js
@@ -3,7 +3,7 @@ var returnHome = document.getElementById("return-home");
 var videoContainer = document.getElementById("video-container");
 var funVideo = document.getElementById("fun-video");
 
-funVideo.onmouseover = function(){mouseOver()};
+/*  funVideo.onmouseover = function(){mouseOver()};  
 
 function mouseOver() {
     returnHome.focus();
@@ -20,9 +20,21 @@ funVideo.onmouseover = function(){
     returnHome.focus();
 }); */
 
+var clicks = 0;
+
 
 function showMenu() {
     var navBar = document.getElementsByClassName("nav-only");
-    navBar[0].classList.toggle("mobile-nav-on")
+
+    if(navBar[0].classList.contains("mobile-nav-on")){
+
+        navBar[0].classList.toggle("mobile-nav-off");
+
+    } else 
+    {    
+        navBar[0].classList.toggle("mobile-nav-on");
+    }
+
+
 
 }
