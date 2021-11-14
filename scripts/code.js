2a38c0888835b6d9747e24bb0098634c6d4486e1
ErlendKH <s349571@oslomet.no>
2021-11-14 14:56:07 +0100

    b

--- scripts/code.js
+++ scripts/code.js
@@ -1,4 +1,37 @@
 
+var video = document.getElementsByName("vid");
+
+var debug = document.getElementById("debug");
+
+/* $(document).ready(function() {
+    console.log("Ready!");
+
+    debug.textContent = "Document ready!";
+
+    
+}); */
+
+// We use window.onload instead of document.ready because the latter is triggered before all images are downloaded
+$(window).on("load", function(){
+
+    debug.textContent = "Document ready!";
+
+    var video_jq = $('#vid')
+    var video_node = video_jq.get(0);
+
+    video_jq.on("canplaythrough", function(e){
+
+        // Video is downloaded, trigger playing
+        video_node.play();
+
+    });
+
+    // All resources are ready, trigger video downloading
+    video_node.load();
+
+});
+
+
 var root = document.documentElement;
 root.classList.add('has-js');
 
