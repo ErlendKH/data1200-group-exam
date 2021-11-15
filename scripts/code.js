4b4fedcfc1bbda841122d9974e6254d8a7bc0865
ErlendKH <s349571@oslomet.no>
2021-11-16 00:59:54 +0100

    e

--- scripts/code.js
+++ scripts/code.js
@@ -324,12 +324,12 @@ book.addEventListener('click', function() {
 
     dynamic_h2.textContent = "A Journey Into A Strange World";
 
-    dynamic_h3.style.display = "block";
+    /* dynamic_h3.style.display = "block"; */
     dynamic_h3.textContent = "Erlend";
-    dynamic_quote0.textContent = "";
-    
-    dynamic_quote1.textContent = 'The only impossible journey is the one you never begin. - Tony Robbins';
-    dynamic_quote2.textContent = "";
+    dynamic_quote0.textContent = "Do at least one useful thing a day. Keep trying. Have a dream.";
+
+    dynamic_quote1.textContent = 'The only impossible journey is the one you never begin. — Tony Robbins';
+    dynamic_quote2.textContent = "When you cease to dream you cease to live. —Malcolm Forbes";
     /* dynamic_quote3.textContent = ""; */
 
     showPopup();
@@ -344,12 +344,12 @@ pen.addEventListener('click', function() {
 
     dynamic_h2.textContent = "The Lead Character";
 
-    dynamic_h3.style.display = "block";
+    /* dynamic_h3.style.display = "block"; */
     dynamic_h3.textContent = "Patrick";
-    dynamic_quote0.textContent = "";
+    dynamic_quote0.textContent = "Quote...";
 
-    dynamic_quote1.textContent = "Many of life's failures are people who did not realize how close they were to success when they gave up. - Thomas A. Edison";
-    dynamic_quote2.textContent = "The way to get started is to quit talking and begin doing. - Walt Disney";
+    dynamic_quote1.textContent = "Many of life's failures are people who did not realize how close they were to success when they gave up. — Thomas A. Edison";
+    dynamic_quote2.textContent = "The way to get started is to quit talking and begin doing. — Walt Disney";
     /* dynamic_quote3.textContent = ""; */
 
     showPopup();
@@ -364,12 +364,12 @@ smartphone.addEventListener('click', function() {
 
     dynamic_h2.textContent = "The Fall";
 
-    dynamic_h3.style.display = "block";
-    dynamic_h3.textContent = "A";
-    dynamic_quote0.textContent = "Marcin";
+    /* dynamic_h3.style.display = "block"; */
+    dynamic_h3.textContent = "Marcin";
+    dynamic_quote0.textContent = "Quote...";
 
-    dynamic_quote1.textContent = 'The greatest glory in living lies not in never falling, but in rising every time we fall. - Nelson Mandela';
-    dynamic_quote2.textContent = '""';
+    dynamic_quote1.textContent = 'The greatest glory in living lies not in never falling, but in rising every time we fall. — Nelson Mandela';
+    dynamic_quote2.textContent = "Keep calm and carry on. — Winston Churchill";
     /* dynamic_quote3.textContent = ""; */
 
     showPopup();
@@ -384,12 +384,12 @@ laptop.addEventListener('click', function() {
 
     dynamic_h2.textContent = "That's Cute";
 
-    dynamic_h3.style.display = "none";
-    /* dynamic_h3.textContent = "A"; */
-    dynamic_quote0.textContent = "";
+    /* dynamic_h3.style.display = "none"; */
+    dynamic_h3.textContent = "Pom";
+    dynamic_quote0.textContent = "Cookies!";
 
-    dynamic_quote1.textContent = "You know you are on the road to success if you would do your job and not be paid for it. -Oprah Winfrey";
-    dynamic_quote2.textContent = "TTTTTTTT";
+    dynamic_quote1.textContent = "You know you are on the road to success if you would do your job and not be paid for it. — Oprah Winfrey";
+    dynamic_quote2.textContent = "Life is like a coin. You can spend it any way you wish, but you only spend it once. — Lillian Dickson";
     /* dynamic_quote3.textContent = ""; */
 
     showPopup();
@@ -404,12 +404,12 @@ coffee.addEventListener('click', function() {
 
     dynamic_h2.textContent = "A Great Day";
 
-    dynamic_h3.style.display = "block";
+    /* dynamic_h3.style.display = "block"; */
     dynamic_h3.textContent = "Majureka";
-    dynamic_quote0.textContent = "";
+    dynamic_quote0.textContent = "Quote...";
 
-    dynamic_quote1.textContent = "I failed my way to success. -Thomas Edison";
-    dynamic_quote2.textContent = '""';
+    dynamic_quote1.textContent = "I failed my way to success. — Thomas Edison";
+    dynamic_quote2.textContent = "The whole secret of a successful life is to find out what is one’s destiny to do, and then do it. — Henry Ford";
     /* dynamic_quote3.textContent = ""; */
 
     showPopup();
@@ -424,12 +424,12 @@ paper.addEventListener('click', function() {
 
     dynamic_h2.textContent = "Beauty";
 
-    dynamic_h3.style.display = "block";
+    /* dynamic_h3.style.display = "block"; */
     dynamic_h3.textContent = "Gulluzar";
-    dynamic_quote0.textContent = "";
+    dynamic_quote0.textContent = "Quote...";
 
-    dynamic_quote1.textContent = "There are no secrets to success. It is the result of preparation, hard work, and learning from failure. - Colin Powell";
-    dynamic_quote2.textContent = "Successful people do what unsuccessful people are not willing to do. Don't wish it were easier; wish you were better. -Jim Rohn";
+    dynamic_quote1.textContent = "There are no secrets to success. It is the result of preparation, hard work, and learning from failure. — Colin Powell";
+    dynamic_quote2.textContent = "Successful people do what unsuccessful people are not willing to do. Don't wish it were easier; wish you were better. — Jim Rohn";
     /* dynamic_quote3.textContent = ""; */
 
     showPopup();
