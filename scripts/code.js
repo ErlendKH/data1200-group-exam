3243d249312a9233ff519422f0f38322fe647955
ErlendKH <s349571@oslomet.no>
2021-11-16 11:40:01 +0100

    i

--- scripts/code.js
+++ scripts/code.js
@@ -50,7 +50,12 @@ function handleVidError(){
 }
 
 $(document).ready(function() {
-/*     console.log("Ready!");
+/*     console.log("Ready!"); */
+
+    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+    let vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
 
     // Particle effects from simeydotme at: https://github.com/simeydotme/sparticles
 
