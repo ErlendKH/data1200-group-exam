bc471dbf857ef4adc953b37f8a3db35586b69ee7
ErlendKH <s349571@oslomet.no>
2021-11-16 12:16:55 +0100

    i

--- scripts/code.js
+++ scripts/code.js
@@ -52,6 +52,8 @@ function handleVidError(){
 $(document).ready(function() {
 /*     console.log("Ready!"); */
 
+    /* var test = vhCheck(); */
+
 /*     // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
     let vh = window.innerHeight * 0.01;
     // Then we set the value in the --vh custom property to the root of the document
