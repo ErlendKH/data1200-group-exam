d8029959a83ff9bc97ef2e5a33df06e7bbcdb892
ErlendKH <s349571@oslomet.no>
2021-11-16 12:58:37 +0100

    n

--- scripts/code.js
+++ scripts/code.js
@@ -52,12 +52,12 @@ function handleVidError(){
 $(document).ready(function() {
 /*     console.log("Ready!"); */
 
-    // We listen to the resize event
+/*     // We listen to the resize event
     window.addEventListener('resize', () => {
         // We execute the same script as before
         let vh = window.innerHeight * 0.01;
         document.documentElement.style.setProperty('--vh', `${vh}px`);
-    });
+    }); */
 
     /* var test = vhCheck(); */
 
