b6c3b704e975718851c59c552b4f8eca41f4b8ba
marcioziomek <s366436@oslomet.no>
2021-11-25 14:20:02 +0100

    fixing mobile nav

--- scripts/code.js
+++ scripts/code.js
@@ -19,3 +19,29 @@ window.onresize = function(){
         mobileNav.style.display = "none";
     }
 }
+
+/* var navOnly = document.getElementsByClassName("nav-only");
+var navMenu = document.getElementById("mobile-menubutton");
+var Main = document.getElementsByClassName("textbox");
+
+navMenu.addEventListener("click", function(){
+
+    if(navOnly[0].style.display == "" || navOnly[0].style.display == "none"){
+        navOnly[0].style.display = "block"
+        navOnly[0].style.animation = "fadeIn 0.5s forwards"
+        Main[0].style.animation = "fadeOut 0.5s forwards"
+       
+
+    } else {
+        navOnly[0].style.animation = "fadeOut 0.5s forwards"
+        navOnly[0].addEventListener("animationend", displayOff);
+        Main[0].style.animation = "fadeIn 0.5s forwards"
+
+    }
+});
+
+  function displayOff(){
+    navOnly[0].style.display = "none";
+    navOnly[0].removeEventListener("animationend", displayOff); 
+}
+*/
