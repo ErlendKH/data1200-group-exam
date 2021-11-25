027d85ca04cbdddb999f13499b9e4b5df6d166a7
marcioziomek <s366436@oslomet.no>
2021-11-25 14:59:38 +0100

    Update code.js

--- scripts/code.js
+++ scripts/code.js
@@ -17,12 +17,12 @@ menuButton.addEventListener('click', function() {
 */
 var navOnly = document.getElementById("mobile-nav");
 var navMenu = document.getElementById("mobile-menubutton");
-var Main = document.getElementById("main");
+var Main = document.getElementsByClassName("textbox");
 
 window.onresize = function(){
     if(window.innerWidth > 960){
         navOnly.style.display = "none"
-        Main.style.animation = "fadeIn 0.5s forwards"
+        Main[0].style.animation = "fadeIn 0.5s forwards"
     }
 }
 
@@ -31,13 +31,13 @@ navMenu.addEventListener("click", function(){
     if(navOnly.style.display == "" || navOnly.style.display == "none"){
         navOnly.style.display = "block"
         navOnly.style.animation = "fadeIn 0.5s forwards"
-        Main.style.animation = "fadeOut 0.5s forwards"
+        Main[0].style.animation = "fadeOut 0.5s forwards"
        
 
     } else {
         navOnly.style.animation = "fadeOut 0.5s forwards"
         navOnly.addEventListener("animationend", displayOff);
-        Main.style.animation = "fadeIn 0.5s forwards"
+        Main[0].style.animation = "fadeIn 0.5s forwards"
 
     }
 });
