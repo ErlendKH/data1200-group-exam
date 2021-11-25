e33a07c205b17693446433766acfc71abf39cda9
ErlendKH <s349571@oslomet.no>
2021-11-25 12:49:21 +0100

    fun

--- scripts/fun.js
+++ scripts/fun.js
@@ -1,8 +1,6 @@
 
 /* Code exclusively for the FUN page. */
 
-/* var debug = document.getElementById("debug"); */
-
 var instructions = document.getElementById("instructions-button");
 var interact = document.getElementById("interact-container");
 var horizontal = document.getElementById("horizontal-prompt-container");
@@ -18,15 +16,6 @@ var laptop = document.getElementById("laptop");
 var coffee = document.getElementById("coffee");
 var paper = document.getElementById("paper");
 
-/* 
-var sparticles_book = document.querySelector(".sparticles-book"); 
-var sparticles_pen = document.querySelector(".sparticles-pen"); 
-var sparticles_smartphone = document.querySelector(".sparticles-smartphone"); 
-var sparticles_laptop = document.querySelector(".sparticles-laptop"); 
-var sparticles_coffee = document.querySelector(".sparticles-coffee"); 
-var sparticles_paper = document.querySelector(".sparticles-paper"); 
- */
-
 var dismiss_button = document.getElementById("dismiss-button");
 var dynamic_h2 = document.getElementById("dynamic-h2");
 var dynamic_h3 = document.getElementById("dynamic-h3");
@@ -35,158 +24,10 @@ var dynamic_quote0 = document.getElementById("dynamic-quote0");
 var dynamic_quote1 = document.getElementById("dynamic-quote1");
 var dynamic_quote2 = document.getElementById("dynamic-quote2");
 
-function showImage(){
-    debug.textContent += " showImage() triggered!";
-    img.style.display = "block";
-}
-
-function hideVid(){
-    debug.textContent += " hideVid() triggered!";
-    vid.style.display = "none";
-}
-
 function handleVidError(){
-    debug.textContent += " handleVidError() triggered!";
-
     img.style.display = "block";
     vid.style.display = "none";
 }
-
-/* 
-$(document).ready(function() {
-
-    // Particle effects from simeydotme at: https://github.com/simeydotme/sparticles
-
-    // BOOK
-
-    let sparticlesBook = new Sparticles( sparticles_book, {count: 25, speed: 5, shape: "random"});
-
-    // Hover events?
-    sparticles_book.addEventListener('mouseenter', e => {
-        sparticlesBook.destroy();
-        setTimeout(function() {
-            sparticlesBook = new Sparticles( sparticles_book, {count: 200, speed: 10, shape: "random"});
-        }, 50);
-      });
-      
-      sparticles_book.addEventListener('mouseleave', e => {
-        sparticlesBook.destroy();
-        setTimeout(function() {
-            sparticlesBook = new Sparticles( sparticles_book, {count: 25, speed: 5, shape: "random"});
-        }, 50);
-    });
-
-    // PEN
-
-    let sparticlesPen = new Sparticles( sparticles_pen, {count: 25, speed: 5, shape: "triangle"});
-
-    sparticles_pen.addEventListener('mouseenter', e => {
-        sparticlesPen.destroy();
-        setTimeout(function() {
-            sparticlesPen = new Sparticles( sparticles_pen, {count: 200, speed: 10, shape: "triangle"});
-        }, 50);
-      });
-      
-      sparticles_pen.addEventListener('mouseleave', e => {
-        sparticlesPen.destroy();
-        setTimeout(function() {
-            sparticlesPen = new Sparticles( sparticles_pen, {count: 25, speed: 5, shape: "triangle"});
-        }, 50);
-    });
-
-    // SMARTPHONE
-
-    let sparticlesSmartphone = new Sparticles( sparticles_smartphone, {count: 25, speed: 5, shape: "circle"});
-
-    sparticles_smartphone.addEventListener('mouseenter', e => {
-        sparticlesSmartphone.destroy();
-        setTimeout(function() {
-            sparticlesSmartphone = new Sparticles( sparticles_smartphone, {count: 200, speed: 10, shape: "circle"});
-        }, 50);
-      });
-      
-      sparticles_smartphone.addEventListener('mouseleave', e => {
-        sparticlesSmartphone.destroy();
-        setTimeout(function() {
-            sparticlesSmartphone = new Sparticles( sparticles_smartphone, {count: 25, speed: 5, shape: "circle"});
-        }, 50);
-    });
-
-    // LAPTOP
-
-    let sparticlesLaptop = new Sparticles( sparticles_laptop, {count: 100, speed: 5, shape: "diamond", color: "gold"});
-
-    sparticles_laptop.addEventListener('mouseenter', e => {
-        sparticlesLaptop.destroy();
-        setTimeout(function() {
-            sparticlesLaptop = new Sparticles( sparticles_laptop, {count: 500, speed: 10, shape: "diamond", color: "gold"});
-        }, 50);
-      });
-      
-      sparticles_laptop.addEventListener('mouseleave', e => {
-        sparticlesLaptop.destroy();
-        setTimeout(function() {
-            sparticlesLaptop = new Sparticles( sparticles_laptop, {count: 100, speed: 5, shape: "diamond", color: "gold"});
-        }, 50);
-    });
-
-    // COFFEE
-
-    let sparticlesCoffee = new Sparticles( sparticles_coffee, {count: 25, speed: 5, shape: "random"});
-
-    sparticles_coffee.addEventListener('mouseenter', e => {
-        sparticlesCoffee.destroy();
-        setTimeout(function() {
-            sparticlesCoffee = new Sparticles( sparticles_coffee, {count: 200, speed: 10, shape: "random"});
-        }, 50);
-      });
-      
-      sparticles_coffee.addEventListener('mouseleave', e => {
-        sparticlesCoffee.destroy();
-        setTimeout(function() {
-            sparticlesCoffee = new Sparticles( sparticles_coffee, {count: 25, speed: 5, shape: "random"});
-        }, 50);
-    });
-
-    // PAPER
-
-    let sparticlesPaper = new Sparticles( sparticles_paper, {count: 25, speed: 5, shape: "triangle"});
-
-    sparticles_paper.addEventListener('mouseenter', e => {
-        sparticlesPaper.destroy();
-        setTimeout(function() {
-            sparticlesPaper = new Sparticles( sparticles_paper, {count: 200, speed: 10, shape: "triangle"});
-        }, 50);
-      });
-      
-      sparticles_paper.addEventListener('mouseleave', e => {
-        sparticlesPaper.destroy();
-        setTimeout(function() {
-            sparticlesPaper = new Sparticles( sparticles_paper, {count: 25, speed: 5, shape: "triangle"});
-        }, 50);
-    });
-
-});
- */
-
-/* 
-// We use window.onload instead of document.ready because the latter is triggered before all images are downloaded
-$(window).on("load", function(){
-
-    // For playing video when done loading. Possibly needed for mobile?
-    // Found on Stack Overflow:
-    // https://stackoverflow.com/questions/54539689/load-video-only-when-document-is-ready-play-it-when-loaded
-
-    var video_jq = $('#vid'); var video_node = video_jq.get(0);
-    video_jq.on("canplaythrough", function(e){
-        video_node.play(); // Video is downloaded, trigger playing
-    });
-    video_node.load(); // All resources are ready, trigger video downloading
-
-});
- */
-
-/* Fun - Clicks: Start */
 
 var background = document.getElementById("popup-background");
 var popup = document.getElementById("popup-box");
@@ -200,7 +41,6 @@ dismiss_button.addEventListener('click', function() {
 /* BOOK */
 
 book.addEventListener('click', function() {
-    /* debug.textContent = "Book clicked!"; */
 
     dynamic_img.src = "/media/book-surreal.jpg";
 
@@ -218,7 +58,6 @@ book.addEventListener('click', function() {
 /* PEN */
 
 pen.addEventListener('click', function() {
-    /* debug.textContent = "Pen clicked!"; */
 
     dynamic_img.src = "/media/pen.jpg";
 
@@ -236,7 +75,6 @@ pen.addEventListener('click', function() {
 /* SMARTPHONE */
 
 smartphone.addEventListener('click', function() {
-    /* debug.textContent = "Smartphone clicked!"; */
 
     dynamic_img.src = "/media/mobile-fall.jpg";
 
@@ -254,7 +92,6 @@ smartphone.addEventListener('click', function() {
 /* LAPTOP */
 
 laptop.addEventListener('click', function() {
-    /* debug.textContent = "Laptop clicked!"; */
 
     dynamic_img.src = "/media/cookie-the-pom-laptop-unsplash.jpg";
 
@@ -272,7 +109,6 @@ laptop.addEventListener('click', function() {
 /* COFFEE */
 
 coffee.addEventListener('click', function() {
-    /* debug.textContent = "Coffee clicked!"; */
 
     dynamic_img.src = "/media/woman-coffee.jpg";
 
@@ -290,7 +126,6 @@ coffee.addEventListener('click', function() {
 /* PAPER */
 
 paper.addEventListener('click', function() {
-    /* debug.textContent = "Paper clicked!"; */
 
     dynamic_img.src = "/media/carolina-garcia-paper.jpg";
 
@@ -307,13 +142,7 @@ paper.addEventListener('click', function() {
 }, false);
 
 background.addEventListener('click', function() {
-    /* debug.textContent = "Background clicked!"; */
-
     hidePopup();
-}, false);
-
-popup.addEventListener('click', function() {
-    /* debug.textContent = "Popup clicked!"; */
 }, false);
 
 function showPopup() {
@@ -336,6 +165,7 @@ instructions.addEventListener('click', function() {
 
 }, false);
 
+/* Text recommending the user to switch to horizontal view on small devices. */
 window.onresize = function(){
     if(window.innerWidth <= 601){
         horizontal.style.display = "block";
@@ -344,6 +174,7 @@ window.onresize = function(){
     }
 }
 
+/* Run once on load. */
 window.onload = function(){
 
     if(window.innerWidth <= 601){
